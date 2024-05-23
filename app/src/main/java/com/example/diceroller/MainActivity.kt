3afdcf17caa6cab322053f148a754f91820fa6ad
pr-Dutta <89duttapranjal@gmail.com>
2024-05-23 10:29:20 +0530

    new commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,6 +12,10 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -49,20 +53,30 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     // The modifier argument ensures that the composables in the Column() function adhere
     // to the constraints called on the modifier instance.
 
-    var result = 1
+    // The remember composable requires a function to be passed.
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         Image(
-            painter = painterResource(id = R.drawable.dice_1),
-            contentDescription = "1"
+            painter = painterResource(imageResource),
+            contentDescription = imageResource.toString()
         )
 
         Spacer(modifier = Modifier.height(16.dp))           // new learning
 
-        Button(onClick = { result = (1..6).random() }) {
+        Button(onClick = { result = (1..6).random() }) {        // new
             Text(text = stringResource(id = R.string.roll))
         }
     }
