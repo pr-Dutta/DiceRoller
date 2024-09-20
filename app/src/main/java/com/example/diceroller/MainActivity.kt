69d1ee96c9864c87db977c3403aebb2ef92560c8
pr-Dutta <89duttapranjal@gmail.com>
2024-09-20 12:14:59 +0530

    new commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -75,14 +75,14 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     // the value of the result variable changes, a recomposition is triggered,
     // the value of the result is reflected, and the UI refreshes.
     var result by remember { mutableStateOf(1) }
-    val imageResource = when (result) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
-    }
+//    val imageResource = when (result) {
+//        1 -> R.drawable.dice_1
+//        2 -> R.drawable.dice_2
+//        3 -> R.drawable.dice_3
+//        4 -> R.drawable.dice_4
+//        5 -> R.drawable.dice_5
+//        else -> R.drawable.dice_6
+//    }
 
     Column(
         modifier = modifier,
